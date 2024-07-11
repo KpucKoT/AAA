85e6da5e7be7eed8c386c6cfc6ac98f27b47f3cd
KpucKoT <167256393+KpucKoT@users.noreply.github.com>
2024-07-11 22:02:26 +0300

    Util.java

--- PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/util/Util.java
+++ PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -18,8 +18,8 @@ public class Util {
     static Connection connection;
 
     private static final String URL = "jdbc:mysql://localhost:3306/mybdtest";
-    private static final String USERNAME = "Kris";
-    private static final String PASSWORD = "Penguin020399";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
 
 
     public static Connection getConnection() {
@@ -48,8 +48,8 @@ public class Util {
                 Properties settings = new Properties();
                 settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
                 settings.put(Environment.URL, "jdbc:mysql://localhost:3306/mybdtest");
-                settings.put(Environment.USER, "Kris");
-                settings.put(Environment.PASS, "Penguin020399");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "root");
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
 
                 settings.put(Environment.SHOW_SQL, "true");
